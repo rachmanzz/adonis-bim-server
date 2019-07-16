70ff72e09a79c5c19cdca98d0b32306d2f222513
Muhammad Abdurrahman <rachman.sd@gmail.com>
2019-07-16 18:48:42 +0700

    update library

--- bim-server.js
+++ bim-server.js
@@ -8,24 +8,32 @@ class BimServer extends Query{
     bimRequest(request) {
         const req = request.get()
         const mret = fReturn(req, [
-                'where', 'orwhere', 'like', 'orlike', 'with', 'between',
+                'where', 'orwhere', 'wherelike', 'with', 'between',
                 'groupby', 'offset', 'limit', 'orderby'
             ])
         for (let key in mret) {
-            this[key](mret)
+            if (key === 'where') this.where(mret[key])
+            if (key === 'orwhere') this.orwhere(mret[key])
+            if (key === 'wherelike') this.wherelike(mret[key])
+            if (key === 'with') this.with(mret[key])
+            if (key === 'between') this.between(mret[key])
+            if (key === 'groupby') this.groupby(mret[key])
+            if (key === 'offset') this.offset(mret[key])
+            if (key === 'limit') this.limit(mret[key])
+            if (key === 'orderby') this.orderby(mret[key])
         }
 
-        if (iString(req.paginate)) {
-            if (/^[0-9]+$/.test(req.paginate)) {
-                this.modal.paginate(parseInt(req.paginate))
-            } else if (/^[0-9]+\|[0-9]+$/.test(req.paginate)) {
-                const m = req.paginate.match(/(^[0-9]+)\|([0-9]+)$/)
-                let key = parseInt(m[1])
-                let val = parseInt(m[2])
-                this.modal.paginate(val, key)
-            }
-        } else if (iString(req.first) && req.first === 'true') this.modal.first()
-        else this.modal.fetch()
+        // if (iString(req.paginate)) {
+        //     if (/^[0-9]+$/.test(req.paginate)) {
+        //         this.modal.paginate(parseInt(req.paginate))
+        //     } else if (/^[0-9]+\|[0-9]+$/.test(req.paginate)) {
+        //         const m = req.paginate.match(/(^[0-9]+)\|([0-9]+)$/)
+        //         let key = parseInt(m[1])
+        //         let val = parseInt(m[2])
+        //         this.modal.paginate(val, key)
+        //     }
+        // } else if (iString(req.first) && req.first === 'true') this.modal.first()
+        // else this.modal.fetch()
     }
 }
 
